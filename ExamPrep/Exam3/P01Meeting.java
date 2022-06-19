3f808aa632f613bbd66fdad74b8f6a3c7e0c5f8a
Martin Krystev <martin.krystev@gmail.com>
2022-06-19 22:06:38 +0300

    Meeting, Throne Conquering

--- ExamPrep/Exam3/P01Meeting.java
+++ ExamPrep/Exam3/P01Meeting.java
@@ -1,5 +1,143 @@
 package com.company.ExamPrep.Exam3;
 
-public class P01Meeting {
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.Scanner;
 
+public class P01Meeting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] males = scanner.nextLine().split("\\s+");
+        String[] females = scanner.nextLine().split("\\s+");
+
+        ArrayDeque<Integer> maleStack = new ArrayDeque<>();
+        for (int i = 0; i < males.length; i++) {
+            maleStack.push(Integer.parseInt(males[i]));
+        }
+        ArrayDeque<Integer> femaleQueue = new ArrayDeque<>();
+        for (int i = 0; i < females.length; i++) {
+            femaleQueue.offer(Integer.parseInt(females[i]));
+        }
+
+        int matchesCount = 0;
+        while (maleStack.size() > 0 && femaleQueue.size() > 0) {
+
+            int currentMale = maleStack.peek();
+            int currentFemale = femaleQueue.peek();
+            // checks if n > 0
+            while (currentMale <= 0) {
+                maleStack.pop();
+                if (maleStack.size() > 0) {
+                    currentMale = maleStack.peek();
+                }
+            }
+            if (oneIsEmpty(maleStack, femaleQueue)) break;
+            while (currentFemale <= 0) {
+                femaleQueue.poll();
+                if (femaleQueue.size() > 0) {
+                    currentFemale = femaleQueue.peek();
+                }
+            }
+            // checks if n % 25 == 0
+            if (oneIsEmpty(maleStack, femaleQueue)) break;
+            while (currentMale % 25 == 0) {
+                maleStack.pop();
+                if (maleStack.size() > 0) {
+                    maleStack.pop();
+                }
+                if (maleStack.size() > 0) {
+                    currentMale = maleStack.peek();
+                } else {
+                    break;
+                }
+            }
+            if (oneIsEmpty(maleStack, femaleQueue)) break;
+            while (currentFemale % 25 == 0) {
+                femaleQueue.poll();
+                if (femaleQueue.size() > 0) {
+                    femaleQueue.poll();
+                }
+                if (femaleQueue.size() > 0) {
+                    currentFemale = femaleQueue.peek();
+                } else {
+                    break;
+                }
+            }
+            // checks if the collections still have elements
+            if (oneIsEmpty(maleStack, femaleQueue)) break;
+            // checks if male == female
+            if (currentMale != currentFemale) {
+                if (currentMale - 2 > 0) {
+                    currentMale = maleStack.pop();
+                    maleStack.push(currentMale - 2);
+                    femaleQueue.poll();
+                } else {
+                    maleStack.pop();
+                    femaleQueue.poll();
+                }
+            } else {
+                matchesCount++;
+                maleStack.pop();
+                femaleQueue.poll();
+            }
+        }
+
+        System.out.printf("Matches: %d%n", matchesCount);
+        printMales(maleStack);
+        printFemales(femaleQueue);
+    }
+
+    private static boolean oneIsEmpty(ArrayDeque<Integer> maleStack, ArrayDeque<Integer> femaleQueue) {
+        return maleStack.size() <= 0 || femaleQueue.size() <= 0;
+    }
+
+    private static void printMales(ArrayDeque<Integer> maleStack) {
+        if (maleStack.size() <= 0) {
+            System.out.println("Males left: none");
+        } else {
+//            System.out.print("Males left: ");
+//            while (maleStack.size() > 0) {
+//                System.out.print(maleStack.poll());
+//                if (maleStack.size() > 0) {
+//                    System.out.print(", ");
+//                } else {
+//                    System.out.println();
+//                }
+//            }
+
+            StringBuilder sb = new StringBuilder();
+            while (maleStack.size() > 0) {
+                sb.append(maleStack.poll());
+                if (maleStack.size() > 0) {
+                    sb.append(", ");
+                }
+            }
+            System.out.println("Males left: " + sb);
+        }
+    }
+
+    private static void printFemales(ArrayDeque<Integer> femaleQueue) {
+        if (femaleQueue.size() <= 0) {
+            System.out.println("Females left: none");
+        } else {
+//            System.out.print("Females left: ");
+//            while (femaleQueue.size() > 0) {
+//                System.out.print(femaleQueue.poll());
+//                if (femaleQueue.size() > 0) {
+//                    System.out.print(", ");
+//                } else {
+//                    System.out.println();
+//                }
+//            }
+            StringBuilder sb = new StringBuilder();
+            while (femaleQueue.size() > 0) {
+                sb.append(femaleQueue.poll());
+                if (femaleQueue.size() > 0) {
+                    sb.append(", ");
+                }
+            }
+            System.out.println("Females left: " + sb);
+        }
+    }
 }
--- ExamPrep/Exam3/P02ThroneConquering.java
+++ ExamPrep/Exam3/P02ThroneConquering.java
@@ -1,4 +1,155 @@
 package com.company.ExamPrep.Exam3;
 
+import java.util.Scanner;
+
 public class P02ThroneConquering {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int energy = Integer.parseInt(scanner.nextLine());
+        int rows = Integer.parseInt(scanner.nextLine());
+        char[][] field = readCharMatrix(rows, scanner);
+
+        int parisRow = -1;
+        int parisCol = -1;
+
+        for (int row = 0; row < field.length; row++) {
+            for (int col = 0; col < field[row].length; col++) {
+                if (field[row][col] == 'P') {
+                    parisRow = row;
+                    parisCol = col;
+                }
+            }
+        }
+
+        boolean isAbducted = false;
+
+        while (energy > 0) {
+            String[] command = scanner.nextLine().split("\\s+");
+
+            String direction = command[0];
+            int spawnRow = Integer.parseInt(command[1]);
+            int spawnCol = Integer.parseInt(command[2]);
+
+            field[spawnRow][spawnCol] = 'S';
+            energy--;
+
+            switch (direction) {
+                case "up":
+                    if (parisRow - 1 >= 0) {
+                        field[parisRow][parisCol] = '-';
+                        parisRow--;
+                        if (field[parisRow][parisCol] == 'S') { // Spartan
+                            energy -= 2;
+                            if (energy <= 0) {
+                                field[parisRow][parisCol] = 'X';
+                            } else {
+                                field[parisRow][parisCol] = 'P';
+                            }
+                        } else if (field[parisRow][parisCol] == 'H') { // Helen
+                            field[parisRow][parisCol] = '-';
+                            isAbducted = true;
+                            break;
+                        } else { // empty slot
+                            field[parisRow][parisCol] = 'P';
+                        }
+                    }
+                    break;
+                case "down":
+                    if (parisRow + 1 < field.length) {
+                        field[parisRow][parisCol] = '-';
+                        parisRow++;
+
+                        if (field[parisRow][parisCol] == 'S') { // Spartan
+                            energy -= 2;
+                            if (energy <= 0) {
+                                field[parisRow][parisCol] = 'X';
+                            } else {
+                                field[parisRow][parisCol] = 'P';
+                            }
+                        } else if (field[parisRow][parisCol] == 'H') { // Helen
+                            field[parisRow][parisCol] = '-';
+                            isAbducted = true;
+                            break;
+                        } else { // empty slot
+                            field[parisRow][parisCol] = 'P';
+                        }
+                    }
+                    break;
+                case "left":
+                    if (parisCol - 1 >= 0) {
+                        field[parisRow][parisCol] = '-';
+                        parisCol--;
+                        if (field[parisRow][parisCol] == 'S') { // Spartan
+                            energy -= 2;
+                            if (energy <= 0) {
+                                field[parisRow][parisCol] = 'X';
+                            } else {
+                                field[parisRow][parisCol] = 'P';
+                            }
+                        } else if (field[parisRow][parisCol] == 'H') { // Helen
+                            field[parisRow][parisCol] = '-';
+                            isAbducted = true;
+                            break;
+                        } else { // empty slot
+                            field[parisRow][parisCol] = 'P';
+                        }
+                    }
+                    break;
+                case "right":
+                    if (parisCol + 1 < field[parisRow].length) {
+                        field[parisRow][parisCol] = '-';
+                        parisCol++;
+                        if (field[parisRow][parisCol] == 'S') { // Spartan
+                            energy -= 2;
+                            if (energy <= 0) {
+                                field[parisRow][parisCol] = 'X';
+                            } else {
+                                field[parisRow][parisCol] = 'P';
+                            }
+                        } else if (field[parisRow][parisCol] == 'H') { // Helen
+                            field[parisRow][parisCol] = '-';
+                            isAbducted = true;
+                            break;
+                        } else { // empty slot
+                            field[parisRow][parisCol] = 'P';
+                        }
+                    }
+                    break;
+            }
+
+            if (isAbducted) {
+                break;
+            }
+
+        }
+
+        if (energy <= 0) {
+            field[parisRow][parisCol] = 'X';
+            System.out.printf("Paris died at %d;%d.%n", parisRow, parisCol);
+        } else {
+            System.out.printf("Paris has successfully abducted Helen! Energy left: %d%n", energy);
+        }
+
+        printMatrix(field);
+    }
+
+    public static char[][] readCharMatrix(int rows, Scanner scanner) {
+        char[][] matrix = new char[rows][];
+        for (int row = 0; row < rows; row++) {
+            char[] arr = scanner.nextLine().toCharArray();
+            matrix[row] = arr;
+        }
+        return matrix;
+    }
+
+    public static void printMatrix(char[][] matrix) {
+        for (char[] arr : matrix) {
+            for (char el : arr) {
+                System.out.print(el);
+            }
+            System.out.println();
+        }
+    }
+
 }
